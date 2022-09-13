a04f11dafeccf23e2b22747048ad75bfe409ad41
Abdulaziz Alsafadi <218110169@psu.edu.sa>
2022-09-13 08:45:47 +0300

    branch2

---
+++ DEMO3.java
@@ -0,0 +1,7 @@
+public class DEMO {
+    public static void main(String[] args) {
+        System.out.println("Welcome to DEVOPS");
+        System.out.println("Welcome to fun");
+	System.out.println("This is branch2, which is a copy of DEMO2.java file");   
+ }
+}
