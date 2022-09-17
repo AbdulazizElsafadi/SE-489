039ce2a496900ab79d479937e4c9e8012991d7d9
Abdulaziz Alsafadi <218110169@psu.edu.sa>
2022-09-17 08:40:32 +0300

    for loop added

--- DEMO2.java
+++ DEMO2.java
@@ -3,5 +3,10 @@ public class DEMO {
         System.out.println("Welcome to DEVOPS");
         System.out.println("Welcome to fun");
 	System.out.println("This is branch 1, which is a copy of DEMO.java file");   
+
+	for(int i =0; i < 10; i++) {
+		System.out.println("Welcome to Java");
+	}
  }
+
 }
