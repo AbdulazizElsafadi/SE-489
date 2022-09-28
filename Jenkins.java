33d4dd5fb28777452363c5e74c779b69c66b45ab
Abdulaziz Alsafadi <218110169@psu.edu.sa>
2022-09-28 09:44:45 +0300

    3rd try to configure Jenkins with git

--- Jenkins.java
+++ Jenkins.java
@@ -3,5 +3,6 @@ public class Jenkins {
 public static void main(String[] args) {
 System.out.println("Configuring Jenkins with git");
 System.out.println("After Configuration, Now we will see the how Jenkins is going to work with Git");
+System.out.println("3rd try");
 }
 }
