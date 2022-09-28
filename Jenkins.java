9920e9467d5e64b1c4187f611e96db61e477c1a1
Abdulaziz Alsafadi <218110169@psu.edu.sa>
2022-09-28 10:04:19 +0300

    6th try

--- Jenkins.java
+++ Jenkins.java
@@ -5,5 +5,6 @@ System.out.println("Configuring Jenkins with git");
 System.out.println("After Configuration, Now we will see the how Jenkins is going to work with Git");
 System.out.println("3rd try");
 System.out.println("4th try");
+System.out.println("6th try");
 }
 }
