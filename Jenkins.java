d6eea01824ce849ab295469d851981a18c7fb47a
Abdulaziz Alsafadi <218110169@psu.edu.sa>
2022-09-28 09:26:23 +0300

    After Configuration

--- Jenkins.java
+++ Jenkins.java
@@ -2,5 +2,6 @@ public class Jenkins {
 
 public static void main(String[] args) {
 System.out.println("Configuring Jenkins with git");
+System.out.println("After Configuration, Now we will see the how Jenkins is going to work with Git");
 }
 }
